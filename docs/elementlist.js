7f1f947cf5074a840b184439907792f554f5e109
Carlos Henrique <carloshlfzanon@gmail.com>
2017-06-05 14:30:44 -0300

    Adicionado documentação em HTML com ApiGen

---
+++ docs/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","CHZApp\\Application"],["c","CHZApp\\AssetParser"],["c","CHZApp\\Component"],["c","CHZApp\\ConfigComponent"],["c","CHZApp\\Controller"],["c","CHZApp\\EloquentManager"],["c","CHZApp\\HttpClient"],["c","CHZApp\\Mailer"],["c","CHZApp\\Middleware"],["c","CHZApp\\Router"],["c","CHZApp\\Session"],["c","CHZApp\\SmartyView"]];
