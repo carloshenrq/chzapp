fc189bc02a0f8098e3720ea399aa6e3a01a16608
Carlos Henrique <carloshlfzanon@gmail.com>
2018-09-04 11:53:45 -0300

    Adicionado interface para gerenciamento de eventos

---
+++ lib/CHZApp/IEventHandler.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * BSD 3-Clause License
+ * 
+ * Copyright (c) 2017, Carlos Henrique
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ ** Redistributions of source code must retain the above copyright notice, this
+ *  list of conditions and the following disclaimer.
+ * 
+ ** Redistributions in binary form must reproduce the above copyright notice,
+ *  this list of conditions and the following disclaimer in the documentation
+ *  and/or other materials provided with the distribution.
+ * 
+ ** Neither the name of the copyright holder nor the names of its
+ *  contributors may be used to endorse or promote products derived from
+ *  this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+namespace CHZApp;
+
+/**
+ * Gerenciador de eventos.
+ */
+interface IEventHandler
+{
+	/**
+	 * Trata todos os eventos de classe.
+	 * on_init
+	 * on_test
+	 * on_xxxx
+	 */
+	public function parseEventMethods();
+
+	/**
+	 * Vincula o callback com o evento.
+	 *
+	 * @param string $event Nome do evento que será disparado.
+	 * @param callback $callback Rotina que será executada.
+	 */
+	public function addEventListener($event, $callback);
+
+	/**
+	 * Dispara o evento informado.
+	 * Os argumentos sub-sequentes serão usados para passar ao método.
+	 *
+	 * @param string $event
+	 */
+	public function trigger($event);
+}
