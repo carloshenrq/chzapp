10141e77655712afb8c4dee5e7756579240ef6ba
Carlos Zanon <carloshlfzanon@gmail.com>
2022-01-17 16:48:42 -0300

    Removido definições

--- lib/CHZApp/Controller.php
+++ lib/CHZApp/Controller.php
@@ -1,24 +1,24 @@
 <?php
 /**
  * BSD 3-Clause License
- * 
+ *
  * Copyright (c) 2017, Carlos Henrique
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  ** Redistributions of source code must retain the above copyright notice, this
  *  list of conditions and the following disclaimer.
- * 
+ *
  ** Redistributions in binary form must reproduce the above copyright notice,
  *  this list of conditions and the following disclaimer in the documentation
  *  and/or other materials provided with the distribution.
- * 
+ *
  ** Neither the name of the copyright holder nor the names of its
  *  contributors may be used to endorse or promote products derived from
  *  this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -96,7 +96,7 @@ abstract class Controller extends Component
      */
     public function __construct(IApplication $application)
     {
-        // Inicializa as variaveis de rotas customizadas e 
+        // Inicializa as variaveis de rotas customizadas e
         // restrições de rota.
         $this->customRoutes = [];
         $this->restrictionRoutes = [];
@@ -119,7 +119,7 @@ abstract class Controller extends Component
 
     /**
      * Realiza o tratamento das rotas que foram comentadas.
-     * 
+     *
      * @return void
      */
     private function parseDocRoutes()
@@ -345,7 +345,7 @@ abstract class Controller extends Component
     }
 
     /**
-     * Faz o tratamento da rota e retorna o caminho correto 
+     * Faz o tratamento da rota e retorna o caminho correto
      *  para retorno dos dados.
      *
      * @param string $route Dados de rota.
@@ -437,12 +437,9 @@ abstract class Controller extends Component
         // Após obter os parametros de rotas, obtém também todos os dados
         // Enviados pela requisição para poder enviar ao método caso
         // Necessário.
-        $get        = $request->getQueryParams();   // dados do $_GET 
+        $get        = $request->getQueryParams();   // dados do $_GET
         $post       = $request->getParsedBody();    // dados de $_POST
         $files      = $request->getUploadedFiles(); // dados de $_FILES
-
-        // Remove as definições de $_POST, $_GET, $_REQUEST e $_FILES
-        unset($_POST, $_GET, $_REQUEST, $_FILES);
 
         // Define os dados recebidos.
         $this->setReceivedData($get, $post, $files);
